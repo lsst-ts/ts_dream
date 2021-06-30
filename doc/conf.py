a277d4ba5a1c83e805de2f42754f938e3cec6bbf
wvreeven <wvanreeven@lsst.org>
2021-06-30 16:58:01 -0400

    DM-29906: Added mypy support for pytest.

--- doc/conf.py
+++ doc/conf.py
@@ -4,9 +4,9 @@ from documenteer.conf.pipelinespkg import *  # noqa
 
 
 project = "ts_dream"
-html_theme_options["logotext"] = project  # noqa
+html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
 
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
-intersphinx_mapping["ts_tcpip"] = ("https://ts-tcpip.lsst.io", None)  # noqa
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_tcpip"] = ("https://ts-tcpip.lsst.io", None)  # type: ignore # noqa
